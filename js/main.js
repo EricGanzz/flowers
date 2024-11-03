b69553e2711be6a6b4384e5bd52d37dfd0d5d073
EricDev <90078706+EricGanzz@users.noreply.github.com>
2024-11-03 18:10:42 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('For You').split('')
+    const titles = ('For You My Dindaaaaa').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
